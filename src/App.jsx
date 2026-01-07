40054b3c068ab594614d34cadea55fc0149cc699
unknown <Admin@instructor-224.LABSFC.COM>
2026-01-07 10:32:42 +0500

    first project

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+    <nav>
+      <img src="/vite.svg" alt="" />
+      <ul>
+        <li>Link</li>
+        <li>Link</li>
+        <li>Link</li>
+        <li>Link</li>
+        <li>Link</li>
+      </ul>
+      <div className="act-btns">
+        <button>Login</button>
+        <button>Signup</button>
+      </div>
+    </nav>
+
+    <div className="banner">
+      <img src="https://marketplace.canva.com/EAFOSCtQ7p0/2/0/1600w/canva-orange-and-white-modern-find-your-dream-house-banner-6F7OhzOb6W8.jpg" alt="" />
+    </div>
+      
+    </>
+  )
+}
+
+export default App
